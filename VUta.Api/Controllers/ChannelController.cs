20a2b3ac73145cf1a76f35cd3788e4b30037f268
Illyaz <illyaz.ptts@gmail.com>
2023-05-01 00:00:56 +0700

    Support add channel batch

--- VUta.Api/Controllers/ChannelController.cs
+++ VUta.Api/Controllers/ChannelController.cs
@@ -13,13 +13,16 @@ namespace VUta.Api.Controllers
     public class ChannelController : ControllerBase
     {
         private readonly ILogger<ChannelController> _logger;
-        private readonly IRequestClient<AddChannel> _bus;
+        private readonly IRequestClient<AddChannel> _addChannelClient;
+        private readonly IBus _bus;
 
         public ChannelController(
             ILogger<ChannelController> logger,
-            IRequestClient<AddChannel> bus)
+            IRequestClient<AddChannel> addChannelClient,
+            IBus bus)
         {
             _logger = logger;
+            _addChannelClient = addChannelClient;
             _bus = bus;
         }
 
@@ -28,8 +31,16 @@ namespace VUta.Api.Controllers
         public async Task<IActionResult> AddChannelAsync(
             string id)
         {
-            var result = await _bus.GetResponse<AddChannelResult>(new(id));
+            var result = await _addChannelClient.GetResponse<AddChannelResult>(new(id));
             return Ok(result.Message);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddChannelsAsync(
+            [FromBody] string[] ids)
+        {
+            await _bus.PublishBatch(ids.Select(id => new AddChannel(id)));
+            return Accepted();
         }
     }
 }
